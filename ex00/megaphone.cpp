0c1061032790ef5cee357e5fc963c0a816dcb930
wees-s <wesleyalexandre3946@gmail.com>
2026-02-10 12:51:08 -0300

    fix cast toupper loop

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,9 +5,9 @@ int main(int ac, char **av){
     if (ac < 2)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else{
-        for (short i = 0; av[i]; i++)
-            for (short j = 0; av[i][j]; j++)
-                std::cout << toupper(av[i][j]);
+        for (int i = 1; av[i]; i++)
+            for (int j = 0; av[i][j]; j++)
+                std::cout << static_cast<char>(toupper(static_cast<unsigned char>(av[i][j])));
     }
     std::cout << std::endl;
     return (0);
